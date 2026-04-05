4841f47c7002ba25d04ff6a4c5695ec49bebc47f
criscahu <christian.catamuscay.h@gmail.com>
2026-04-05 10:52:02 -0500

    first commit

---
+++ src/main/java/com/criscahub/erp_lite/ErpLiteApplication.java
@@ -0,0 +1,13 @@
+package com.criscahub.erp_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpLiteApplication.class, args);
+	}
+
+}
